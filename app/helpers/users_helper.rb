e0c5e9c402f30cc4b173157289c3fd2324b883b6
z3phy <stephenmcnelly@gmail.com>
2017-03-31 14:21:57 +0100

    Home page ui improvements

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,7 +4,7 @@ module UsersHelper
       "<i class='fa fa-code'></i>".html_safe
     elsif @user.profile.job_title == "Entrepreneur"
       "<i class='fa fa-lightbulb-o'></i>".html_safe
-    elsif @use.profile.job_title == "Investor"
+    elsif @user.profile.job_title == "Investor"
       "<i class='fa fa-dollar'></i>".html_safe
     end
   end
