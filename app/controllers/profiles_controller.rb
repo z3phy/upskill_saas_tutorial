6a12535d2ee5ee24c69982a0ecc3f6f596b9fd7d
z3phy <stephenmcnelly@gmail.com>
2017-03-31 17:42:43 +0100

    Added profile update action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -14,7 +14,7 @@ class ProfilesController < ApplicationController
     @profile = @user.build_profile( profile_params )
     if @profile.save
       flash[:success] = 'Profile updated!'
-      redirect_to user_path( params[:user_id] )
+      redirect_to user_path( id: params[:user_id] )
     else
       render action: :new
       flash[:warning] = 'Sorry, something went wrong!'
@@ -26,7 +26,23 @@ class ProfilesController < ApplicationController
     # Edit profile linked to this specific user
     @user = User.find( params[:user_id] )
     @profile = @user.profile
+  end
 
+  # PUT to /users/:user_id/profile
+  def update
+    # Retrieve the user from the user database
+    @user = User.find( params[:user_id] )
+    # Retrieve that users profile
+    @profile = @user.profile
+    # Mass assign edited profile attributes and save
+    if @profile.update_attributes(profile_params)
+      flash[:success] = 'Profile Updated!'
+      # Redirect user to their profile page
+      redirect_to user_path(id: params[:user_id])
+    else
+      render action: :edit
+      flash[:warning] = 'Sorry, something went wrong!'
+    end
   end
 
   private 
